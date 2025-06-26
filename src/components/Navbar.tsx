5cad0f91f1308b96f417fcd365cfb185ca514005
Mobinafshari <mma137813781378@gmail.com>
2025-06-26 16:20:25 +0330

    fix: some changes for project 's starter

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="w-full  py-4 bg-secondary text-base-white">
+      <h1 className="text-xl mx-auto text-center text-inherit">
+        Hello Friend!
+      </h1>
+    </nav>
+  );
+}
+
+export default Navbar;
